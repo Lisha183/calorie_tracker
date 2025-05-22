c59e6f9ac9870ad9c3cd0bcadf6233f0513fdf1a
Alicia <chegealicia8@gmail.com>
2025-05-23 02:04:36 +0300

    templates

--- myapp/views.py
+++ myapp/views.py
@@ -9,8 +9,7 @@ from django.utils import timezone
 from datetime import date
 from datetime import datetime
 from .filters import FoodFilter
-import pprint
-from django.template import engines
+
 
 @login_required(login_url='login')
 
@@ -77,7 +76,7 @@ def LoginPage(request):
 			else:
 				messages.info(request,'Username or password is incorrect')
 		context = {}
-		pprint.pprint(engines['django'].engine.dirs)
+		
 		return render(request,'login.html',context)
 
 def LogOutPage(request):
